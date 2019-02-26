3a09bd5b7991e5117b07e4c548de200938f6b406
oppahansi <oppahansi@gmail.com>
2019-02-26 17:36:04 +0100

    Initial Commit

---
+++ MEBetterSaves/App.xaml.cs
@@ -0,0 +1,91 @@
+﻿using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json.Linq;
+using System;
+using System.IO;
+using System.Linq;
+using System.Windows;
+
+namespace MEBetterSaves
+{
+    public partial class App : Application
+    {
+        private IConfigurationRoot Config;
+
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            Config = BuildConfig();
+
+            SetConfigDefaults();
+            CreateFolders();
+
+            // Create the startup window
+            MainWindow wnd = new MainWindow(Config);
+
+
+
+            wnd.Show();
+        }
+
+        private IConfigurationRoot BuildConfig()
+        {
+            try
+            {
+                var builder = new ConfigurationBuilder()
+                    .SetBasePath(AppContext.BaseDirectory)
+                    .AddJsonFile("config.json", optional: false, reloadOnChange: true);
+
+                return builder.Build();
+            }
+            catch
+            {
+                throw new FileNotFoundException();
+            }
+        }
+
+        private void SetConfigDefaults()
+        {
+            var defaultsLoaded = int.Parse(Config["defaultsLoaded"]);
+            var userFolder = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
+
+            if (defaultsLoaded == 1)
+                return;
+            else
+            {
+                JObject configObj = JObject.Parse(File.ReadAllText(Path.Combine(AppContext.BaseDirectory, "config.json")));
+
+                if (!Directory.Exists($"{userFolder + "\\Saved Games\\metro exodus\\"}"))
+                    configObj["metroSaveGamesFolderExists"] = 0;
+                else
+                {
+                    var path = userFolder + Config["defaultSaveGamesFolder"];
+                    var metroSaveFilesDir = Directory.GetDirectories($"{userFolder + Config["defaultSaveGamesFolder"]}");
+
+                    if (metroSaveFilesDir.Length == 0 || metroSaveFilesDir.Length > 1)
+                        configObj["metroSaveGamesFolderExists"] = 0;
+                    else
+                    {
+                        configObj["metroSaveGamesFolder"] = metroSaveFilesDir[0];
+                        configObj["betterSaveGamesFolder"] = userFolder + Config["defaultBetterSaveGamesFolder"];
+                    }
+                }
+
+                configObj["defaultsLoaded"] = 1;
+
+                File.WriteAllText(Path.Combine(AppContext.BaseDirectory, "config.json"), configObj.ToString());
+            }
+        }
+
+        private void CreateFolders()
+        {
+            JObject configObj = JObject.Parse(File.ReadAllText(Path.Combine(AppContext.BaseDirectory, "config.json")));
+            var chapterNames = configObj["chapterNames"].ToArray();
+            var betterSaveGamesDir = configObj["betterSaveGamesFolder"];
+
+            foreach (var chapter in chapterNames)
+            {
+                if (!Directory.Exists(betterSaveGamesDir + chapter.ToString()))
+                    Directory.CreateDirectory(betterSaveGamesDir + chapter.ToString());
+            }
+        }
+    }
+}
---
+++ MEBetterSaves/MainWindow.xaml.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json.Linq;
+using System;
+using System.ComponentModel;
+using System.IO;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace MEBetterSaves
+{
+    public partial class MainWindow
+    {
+        private IConfigurationRoot Config;
+
+        public MainWindow(IConfigurationRoot config)
+        {
+            Config = config;
+
+            InitializeComponent();
+
+            var windowPosX = int.Parse(Config["windowPosX"]);
+            var windowPosY = int.Parse(Config["windowPosY"]);
+
+            mainWindow.WindowStartupLocation = WindowStartupLocation.Manual;
+            mainWindow.Left = windowPosX;
+            mainWindow.Top = windowPosY;
+
+            tabControl.SelectedIndex = 0;
+
+            metroSaveGamesPath.Text = Config["metroSaveGamesFolder"];
+            metroBetterSaveGamesPath.Text = Config["betterSaveGamesFolder"];
+
+            Closing += OnWindowClosing;
+
+            PopulateDatesListBox(datesListBox0, Config["betterSaveGamesFolder"] + $"{Config["chapterNames:0"]}");
+
+            DirectoryInfo dinfo = new DirectoryInfo(Config["betterSaveGamesFolder"] + $"{Config["chapterNames:0"]}");
+            DirectoryInfo[] dirs = dinfo.GetDirectories();
+            PopulateListBox((ListBox)FindName("autoSavesListBox" + 0), dirs[0].FullName);
+        }
+
+        private void OnWindowClosing(object sender, CancelEventArgs e)
+        {
+            var windowPosX = Application.Current.MainWindow.Left;
+            var windowPosY = Application.Current.MainWindow.Top;
+
+            if (windowPosX < 0 || windowPosY < 0 || windowPosX > SystemParameters.PrimaryScreenWidth || windowPosY > SystemParameters.PrimaryScreenHeight)
+                return;
+
+            if (int.Parse(Config["windowPosX"]) != windowPosX || int.Parse(Config["windowPosY"]) != windowPosY)
+            {
+                JObject configObj = JObject.Parse(File.ReadAllText(Path.Combine(AppContext.BaseDirectory, "config.json")));
+                configObj["windowPosX"] = windowPosX;
+                configObj["windowPosY"] = windowPosY;
+                File.WriteAllText(Path.Combine(AppContext.BaseDirectory, "config.json"), configObj.ToString());
+            }
+        }
+
+        private void PopulateDatesListBox(ListBox lsb, string Folder, int tabIndex = 0)
+        {
+            DirectoryInfo dinfo = new DirectoryInfo(Folder);
+            DirectoryInfo[] dirs = dinfo.GetDirectories();
+            foreach (var dir in dirs)
+            {
+                lsb.Items.Add(dir.Name);
+            }
+        }
+
+        private void PopulateListBox(ListBox lsb, string Folder)
+        {
+            DirectoryInfo dinfo = new DirectoryInfo(Folder);
+            FileInfo[] Files = dinfo.GetFiles();
+            foreach (FileInfo file in Files)
+            {
+                lsb.Items.Add(file.Name);
+            }
+        }
+    }
+}
